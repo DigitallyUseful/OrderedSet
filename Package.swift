5210d090187f8708e3d6091b474af10c4a05b19d
Joseph Quigley <quigs@hey.com>
2021-09-18 15:44:15 -0400

    Updated package manifest to be compatible with Swift 5.3 / Xcode 12.5

--- Package.swift
+++ Package.swift
@@ -2,5 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "OrderedSet"
+    name: "OrderedSet",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "OrderedSet",
+            targets: ["OrderedSet"])
+    ],
+    targets: [
+        .target(name: "OrderedSet")
+    ]
 )
